cfa7ae750974191e09be4d1665ad14bd00b5b341
Maxime <maxime@keysako.com>
2025-04-14 15:13:11 +0200

    Update package names and versions to reflect rebranding from Keysako Identity to Keysako Connect, including changes to package.json files, build scripts, and HTML templates. Bump versions to 1.1.1 for core, React, and Vue packages, and enhance build configurations for better module exports. (#3)

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,39 +1,49 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
 import terser from '@rollup/plugin-terser';
 
+// Pour obtenir __dirname dans un module ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default {
-  input: 'dist/index.js',
+  input: resolve(__dirname, 'dist/index.js'),
   output: [
     {
-      file: 'dist/index.js',
+      file: resolve(__dirname, 'dist/index.cjs.js'),
       format: 'cjs',
-      exports: 'named',
+      exports: 'auto',
       sourcemap: true,
     },
     {
-      file: 'dist/index.esm.js',
+      file: resolve(__dirname, 'dist/index.esm.js'),
       format: 'esm',
-      exports: 'named',
+      exports: 'auto',
       sourcemap: true,
     },
     {
-      file: 'dist/index.min.js',
+      file: resolve(__dirname, 'dist/index.min.js'),
       format: 'iife',
       name: 'KeysakoIdentity',
+      exports: 'auto',
       plugins: [terser()],
       sourcemap: true,
     },
     {
-      file: 'dist/keysako-connect.min.js',
+      file: resolve(__dirname, 'dist/keysako-connect.min.js'),
       format: 'umd',
       name: 'keysako-connect',
+      exports: 'auto',
       plugins: [terser()],
       sourcemap: true,
       globals: {},
     },
     {
-      file: 'dist/keysako-connect.js',
+      file: resolve(__dirname, 'dist/keysako-connect.js'),
       format: 'umd',
       name: 'keysako-connect',
+      exports: 'auto',
       sourcemap: true,
       globals: {},
     },
